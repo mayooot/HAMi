5a082546e30cd9780460b24fc82a8c970513426b
Harry Li <bertram_li@163.com>
2025-08-18 19:31:30 +0800

    feat(webhook): add additional check for initContainers
    Signed-off-by: Harry Li <bertram_li@163.com>

--- pkg/scheduler/webhook.go
+++ pkg/scheduler/webhook.go
@@ -68,7 +68,7 @@ func (h *webhook) Handle(_ context.Context, req admission.Request) admission.Res
 	}
 	klog.Infof(template, pod.Namespace, pod.Name, pod.UID)
 	hasResource := false
-	for idx, ctr := range pod.Spec.Containers {
+	for idx, ctr := range append(pod.Spec.Containers, pod.Spec.InitContainers...) {
 		c := &pod.Spec.Containers[idx]
 		if ctr.SecurityContext != nil {
 			if ctr.SecurityContext.Privileged != nil && *ctr.SecurityContext.Privileged {
